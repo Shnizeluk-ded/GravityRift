e29a0ac55901ce448341ef063db03f6cc40543e7
Shnizeluk-ded <vsljbalandin@gmail.com>
2026-04-29 16:10:03 +0500

    Update1
    Сделал гравитацию и возможность менять ее направление

---
+++ GravityRift/GravityRift/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GravityRift
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+
+        float x = 400;
+        float y = 100;
+        int radius = 25;
+
+        float speedX = 0;
+        float speedY = 0;
+
+        float gravity = 0.5f;
+
+        //1-вниз, 2-вверх, 3-влево, 4-вправо
+        int gravityDirection = 1;
+
+        Image ballImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string path = Application.StartupPath + "\\ball.jpg";
+
+            ballImage = Image.FromFile(path);
+
+            Size = new Size(800, 600);
+            DoubleBuffered = true;
+
+            KeyDown += Form1_KeyDown;
+            
+            timer.Interval = 16;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            Paint += Form1_Paint;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Down:
+                    gravityDirection = 1; //вниз
+                    break;
+                case Keys.Up:
+                    gravityDirection = 2; //вверх
+                    break;
+                case Keys.Left:
+                    gravityDirection = 3; //влево
+                    break;
+                case Keys.Right:
+                    gravityDirection = 4; //вправо
+                    break;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            switch (gravityDirection)
+            {
+                case 1: //вниз
+                    speedY = speedY + gravity;
+                    break;
+                case 2: //вверх
+                    speedY = speedY - gravity;
+                    break;
+                case 3: //влево
+                    speedX = speedX - gravity;
+                    break;
+                case 4: //вправо
+                    speedX = speedX + gravity;
+                    break;
+            }
+
+            x = x + speedX;
+            y = y + speedY;
+
+            //Проверка столкновений
+            //Левый край
+            if (x - radius < 0)
+            {
+                x = radius;
+                speedX = 0;
+            }
+            //Правый край
+            if (x + radius > ClientSize.Width)
+            {
+                x = ClientSize.Width - radius;
+                speedX = 0;
+            }
+            //Верхний край
+            if (y - radius < 0)
+            {
+                y = radius;
+                speedY = 0;
+            }
+            //Нижний край
+            if (y + radius > ClientSize.Height)
+            {
+                y = ClientSize.Height - radius;
+                speedY = 0;
+            }
+
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(ballImage, x - radius, y - radius, radius * 2, radius * 2);
+        }
+    }
+}
---
+++ GravityRift/GravityRift/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("GravityRift")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GravityRift")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("9f34bff4-583b-4397-8db2-10604ecbfc97")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
